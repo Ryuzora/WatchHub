bda29f0215e178d44c03d415157615f2b317109e
King Ryuzora <ryuzora8@gmail.com>
2026-05-25 14:33:52 +0800

    update workflow

--- frontend/src/app/tmdb-wrapper-test/page.js
+++ frontend/src/app/tmdb-wrapper-test/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from 'react';
+import { forEach } from 'eslint-config-next';
 
 const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
 
