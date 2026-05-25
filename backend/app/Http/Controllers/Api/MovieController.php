a55c61415ec1519fd79da48a055db08380a36ba6
Melonismee <ibnusina9905@gmail.com>
2026-05-25 15:49:30 +0800

    tmdb movie list routes api

--- backend/app/Http/Controllers/Api/MovieController.php
+++ backend/app/Http/Controllers/Api/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Wrappers\TmdbWrapper\Tmdb;
+use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
@@ -18,5 +19,40 @@ class MovieController extends Controller
         $movie = Tmdb::get('movie', (string) $id);
 
         return response()->json($movie);
+    }
+
+    public function popular(Request $request)
+    {
+        return $this->getMovieList($request, 'popular');
+    }
+
+    public function topRated(Request $request)
+    {
+        return $this->getMovieList($request, 'top_rated');
+    }
+
+    public function nowPlaying(Request $request)
+    {
+        return $this->getMovieList($request, 'now_playing');
+    }
+
+    public function upcoming(Request $request)
+    {
+        return $this->getMovieList($request, 'upcoming');
+    }
+
+    private function getMovieList(Request $request, string $category)
+    {
+        $page = (int) $request->query('page', 1);
+        $language = $request->query('language', 'en-US');
+        $region = $request->query('region');
+
+        if ($page < 1) {
+            $page = 1;
+        }
+
+        $movies = Tmdb::movieList($category, $page, $language, $region);
+
+        return response()->json($movies);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MovieController;
 
+Route::get('/movies/popular', [MovieController::class, 'popular']);
+Route::get('/movies/top-rated', [MovieController::class, 'topRated']);
+Route::get('/movies/now-playing', [MovieController::class, 'nowPlaying']);
+Route::get('/movies/upcoming', [MovieController::class, 'upcoming']);
 Route::get('/movies/{id}', [MovieController::class, 'show']);
