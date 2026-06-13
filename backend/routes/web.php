444406ad9c07bfef50da4337a46e90d8ea1f1837
Melonismee <ibnusina9905@gmail.com>
2026-06-13 12:34:19 +0800

    fixing ci

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/dashboard', function () {
+    return Inertia::render('welcome');
+})->middleware(['auth'])->name('dashboard');
+
 require __DIR__.'/admin.php';
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
